974454726c16cd194dac4357aa61d58bd99077a0
Krista Loven <krista.loven.87@gmail.com>
2019-11-08 10:08:21 -0800

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,11 @@
+ian = User.create(name: 'Ian', email: 'i@i.com')
+ren = User.create(name: 'Ren', email: 'r@r.com')
+krista = User.create(name: 'Krista', email: 'k@k.com')
+
+food = Conversation.create(topic: 'food')
+dolphins = Conversation.create(topic: 'dolphins')
+
+Message.create(content: 'I love pizza', is_read: true, conversation: food, user: krista)
+Message.create(content: 'I also love pizza', is_read: true, conversation_id: 1, user: ian)
+Message.create(content: 'I love dolphins', is_read: true, conversation_id: 2, user: ren)
+Message.create(content: 'What are dolphins?', is_read: true, conversation_id: 2, user: ian)
