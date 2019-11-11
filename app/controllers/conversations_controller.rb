51383546ca2f2ccf99860f4c47965f84db393655
ianhunterharold <ianhunterharold@gmail.com>
2019-11-11 13:47:42 -0800

    added routes and validation for conversation

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -1,9 +1,20 @@
 class ConversationsController < ApplicationController
-  
-  def index 
+
+  def index
     render json: Conversation.all
-  end 
+  end
 
+  def create
+    @conversation = Conversation.new(convo_params)
+    @conversation.save
 
+    render json: @conversation
+  end
+
+  private
+
+  def convo_params
+    params.require(:conversation).permit(:topic)
+  end
 
 end
