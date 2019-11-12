84b5d20ba2e88bf0449144439d36e40bdc354a6a
ianhunterharold <ianhunterharold@gmail.com>
2019-11-12 15:01:10 -0800

    start of socket connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,17 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = find_current_user
+    end
+
+    private 
+
+    def find_current_user
+      
+    end 
+
+
   end
 end
