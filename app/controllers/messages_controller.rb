2660f359fc637f5030b946f80eeb22f3e8b3cb24
Krista Loven <krista.loven.87@gmail.com>
2019-11-11 17:13:10 -0800

    create message functionality, serializer update

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,18 @@
 class MessagesController < ApplicationController
+  def index
+    render json: Message.all
+  end
 
-  def index 
-    render json: Message.all 
-  end 
+  def create
+    @message = Message.new(message_params)
+    @message.save
 
+    render json: @message
+  end
 
+  private
 
+  def message_params
+    params.require(:message).permit(:content, :is_read, :user_id, :conversation_id)
+  end
 end
